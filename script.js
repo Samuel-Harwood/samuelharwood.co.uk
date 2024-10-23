dd100fa7ce94d3488bccb91da345a9d60f6a017f
Samuel-Harwood <samuelharwood3823@gmail.com>
2024-10-23 20:18:42 +0100

    Testing
    Added Header

--- script.js
+++ script.js
@@ -71,3 +71,16 @@ document.addEventListener("DOMContentLoaded", () => {
       var y = e.clientY;
       cursor.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
   });
+
+  document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetBlock = document.querySelector(this.getAttribute('href'));
+        targetBlock.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'start'
+        });
+    });
+});
